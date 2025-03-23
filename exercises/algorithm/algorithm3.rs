e3fa5450a16be21ab37beddbcceae22a13fe6523
Heinz <aurelianob808@gmail.com>
2025-03-23 16:39:11 +0800

    update

--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -3,10 +3,19 @@
 	This problem requires you to implement a sorting algorithm
 	you can use bubble sorting, insertion sorting, heap sorting, etc.
 */
-// I AM NOT DONE
+// I AM DONE
 
-fn sort<T>(array: &mut [T]){
-	//TODO
+fn sort<T>(array: &mut [T]) 
+where 
+    T: Ord
+{
+    for i in 0..array.len() {
+        for j in i..array.len() {
+            if array[i] > array[j] {
+                array.swap(i, j);
+            }
+        }
+    }
 }
 #[cfg(test)]
 mod tests {
